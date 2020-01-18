3ec8d6db98a3f65d925f6c3482207061cbe893af
spiralizing <spiralizing@gmail.com>
2020-01-18 17:03:05 -0500

    updating to Julia 1.x

--- src/MidiTS.jl
+++ src/MidiTS.jl
@@ -5,7 +5,7 @@ module MidiTS
 
 
     export get_time_series, get_pitch_seq
-    export pitches_gaps, notas_hertz, get_onon_notes, get_new_voice, get_onoff_notes, silence_gaps, min_voces, max_tempo, rounding, filter_undef
+    export pitches_gaps, notas_hertz, get_onon_notes, serie_notas, get_new_voice, get_onoff_notes, silence_gaps, min_voces, max_tempo, rounding, filter_undef
     export indice, find_more_voices, get_subs
 
 end
